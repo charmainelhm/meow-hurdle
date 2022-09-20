c01d1d3e69b9d287752201163a5707c19d0a9553
Charmaine Lee <charmainelhm@gmail.com>
2022-09-20 10:43:36 +0800

    Check for collision

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const gameScreen = document.querySelector(".screen");
 const character = document.querySelector("#cat");
 
+const originLeft = character.getBoundingClientRect().left;
+const originBottom = character.getBoundingClientRect().bottom;
+
 const jump = () => {
   if (character.classList !== "jump") {
     character.classList.add("jump");
@@ -22,14 +25,30 @@ const generateObstacles = () => {
   gameScreen.appendChild(obstacle);
 };
 
+const checkForCollision = () => {
+  const characterPosition = character.getBoundingClientRect();
+  const obstacle = document.querySelector(".obstacle");
+  if (!obstacle) return;
+
+  const obstaclePosition = obstacle.getBoundingClientRect();
+
+  const obstacleLeft = obstaclePosition.left - originLeft;
+  const characterBottom = originBottom - characterPosition.bottom;
+
+  if (characterBottom < 55 && obstacleLeft < 50 && obstacleLeft > 0)
+    console.log("Collided");
+};
+
 document.addEventListener("keydown", function (e) {
   if (e.code === "Space") {
     jump();
   }
 });
 
-// const setObstacle = setInterval(() => {
-//   const waitTime = Math.random() * 3000;
-//   console.log(waitTime);
-//   setTimeout(generateObstacles, waitTime);
-// }, 3000);
+const setObstacle = setInterval(() => {
+  const waitTime = Math.random() * 2000;
+  console.log(waitTime);
+  setTimeout(generateObstacles, waitTime);
+}, 3000);
+
+const collisionChecker = setInterval(checkForCollision, 100);
