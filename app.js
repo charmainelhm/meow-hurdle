94c3c22674336497f3818dfd354a45bcba224763
Charmaine Lee <charmainelhm@gmail.com>
2022-09-20 12:47:37 +0800

    Add overlay compnent

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const gameScreen = document.querySelector(".screen");
 const character = document.querySelector("#cat");
+const overlay = document.querySelector(".overlay");
 
 const originLeft = character.getBoundingClientRect().left;
 const originBottom = character.getBoundingClientRect().bottom;
+
+const obstacles = [];
+
+let playingGame = false;
+let setObstacle, collisionChecker, timeoutObstacle;
 
 const jump = () => {
   if (character.classList !== "jump") {
@@ -22,6 +28,8 @@ const generateObstacles = () => {
     gameScreen.removeChild(obstacle);
   });
 
+  obstacles.push(obstacle);
+
   gameScreen.appendChild(obstacle);
 };
 
@@ -35,20 +43,53 @@ const checkForCollision = () => {
   const obstacleLeft = obstaclePosition.left - originLeft;
   const characterBottom = originBottom - characterPosition.bottom;
 
-  if (characterBottom < 55 && obstacleLeft < 50 && obstacleLeft > 0)
-    console.log("Collided");
+  if (characterBottom < 55 && obstacleLeft < 50 && obstacleLeft > 0) {
+    clearInterval(setObstacle);
+    clearInterval(collisionChecker);
+    clearTimeout(timeoutObstacle);
+    console.log("obstacle interval: " + setObstacle);
+
+    character.style.animationPlayState = "paused";
+    const allObstacles = document.querySelectorAll(".obstacle");
+
+    allObstacles.forEach((obstacle) => {
+      obstacle.style.animationPlayState = "paused";
+    });
+
+    overlay.classList.remove("hidden");
+    playingGame = false;
+    console.log(playingGame);
+  }
+};
+
+const resetGameElements = () => {
+  const allObstacles = document.querySelectorAll(".obstacle");
+
+  allObstacles.forEach((obstacle) => {
+    obstacle.remove();
+  });
+
+  character.removeAttribute("style");
+};
+
+const initGame = () => {
+  setObstacle = setInterval(() => {
+    const waitTime = Math.random() * 2000;
+    timeoutObstacle = setTimeout(generateObstacles, waitTime);
+  }, 3000);
+
+  collisionChecker = setInterval(checkForCollision, 100);
 };
 
 document.addEventListener("keydown", function (e) {
   if (e.code === "Space") {
-    jump();
+    if (!playingGame) {
+      overlay.classList.add("hidden");
+      playingGame = true;
+      resetGameElements();
+      initGame();
+    } else {
+      jump();
+    }
   }
 });
-
-const setObstacle = setInterval(() => {
-  const waitTime = Math.random() * 2000;
-  console.log(waitTime);
-  setTimeout(generateObstacles, waitTime);
-}, 3000);
-
-const collisionChecker = setInterval(checkForCollision, 100);
